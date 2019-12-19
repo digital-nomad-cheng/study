de4e3991cdf740343484fa7fcd810e448aec944f
vincent <vincentcheng929@gmail.com>
2019-12-19 17:47:54 +0800

    learn c with lldb

---
+++ c/gdb/static.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int natural_generator()
+{
+    int a = 1;
+    static int b = -1;
+    b += 1;
+    return a + b;
+}
+
+int main() 
+{
+    printf("%d\n", natural_generator());
+    printf("%d\n", natural_generator());
+    printf("%d\n", natural_generator());
+    
+    return 0;
+}
