2b74ceb5b406a7364e263a72fe732c292ecb4e82
vincent <vincentcheng929@gmail.com>
2020-01-16 20:54:56 +0800

    code interposition

---
+++ computer_systems/chap07/code_interpos/mymalloc.c
@@ -0,0 +1,36 @@
+#ifdef COMPILETIME
+#include <stdio.h>
+#include <malloc.h>
+
+void *mymalloc(size_t size)
+{
+    void *ptr = malloc(size);
+    printf("malloc (%d) = %p\n", (int)size, ptr);
+    return ptr;
+}
+
+void myfree(void *ptr)
+{
+    free(ptr);
+    printf("free(%p)\n", ptr);
+}
+#endif
+
+#ifdef LINKTIME
+#include <stdio.h>
+void *__real_malloc(size_t size);
+void __real_free(void *ptr);
+
+void *__wrap_malloc(size_t size)
+{
+    void *ptr = __real_malloc(size);
+    printf("malloc (%d) = %p\n", (int)size, ptr);
+    return ptr;
+}
+
+void __wrap_free(void *ptr)
+{
+    __real_free(ptr);
+    printf("free(%p)\n", ptr);
+}
+#endif
