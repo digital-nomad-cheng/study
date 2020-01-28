d392dd51933a6e30e7bac38386f3d9ec7e15998a
vincent <vincentcheng@929@gmail.com>
2020-01-28 21:19:40 +0800

    udacity c++

--- cpp/udacity_cpp/04_memory_management/garbage_collector/main.cpp
+++ cpp/udacity_cpp/04_memory_management/garbage_collector/main.cpp
@@ -4,8 +4,18 @@
 int main()
 {
     Pointer<int> p = new int(19);
+    
+    p.showList();
     p = new int(21);
+    p.showList();
     p = new int(28);
-
+    p.showList();
+    
+    Pointer<int> q = new int(9);
+    std::cout << "q.showList(): " << std::endl;
+    q.showList();
+    p = q;
+    std::cout << "p.showList(): " << std::endl;
+    p.showList();
     return 0;
 }
