435c42d3493d9b1bfb2ae473dd6e1ca781fd38e7
vincent <vincentcheng@929@gmail.com>
2020-02-02 00:07:08 +0800

    udacity c++

--- cpp/udacity_cpp/05_concurrency/concurrent_traffic_simulation/src/vehicle.cpp
+++ cpp/udacity_cpp/05_concurrency/concurrent_traffic_simulation/src/vehicle.cpp
@@ -33,7 +33,8 @@ void Vehicle::drive()
 {
     // print id of the current thread
     std::unique_lock<std::mutex> lck(_mtx);
-    std::cout << "Vehicle #" << _id << "::drive: thread id = " << std::this_thread::get_id() << std::endl;
+    std::cout << "Vehicle #" << _id << "::drive: thread id = " 
+             << std::this_thread::get_id() << std::endl;
     lck.unlock();
 
     // initalize variables
@@ -49,7 +50,8 @@ void Vehicle::drive()
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
         // compute time difference to stop watch
-        long timeSinceLastUpdate = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - lastUpdate).count();
+        long timeSinceLastUpdate = std::chrono::duration_cast<std::chrono::milliseconds>(
+                        std::chrono::system_clock::now() - lastUpdate).count();
         if (timeSinceLastUpdate >= cycleDuration)
         {
             // update position with a constant velocity motion model
@@ -61,7 +63,9 @@ void Vehicle::drive()
             // compute current pixel position on street based on driving direction
             std::shared_ptr<Intersection> i1, i2;
             i2 = _currDestination;
-            i1 = i2->getID() == _currStreet->getInIntersection()->getID() ? _currStreet->getOutIntersection() : _currStreet->getInIntersection();
+            i1 = i2->getID() == _currStreet->getInIntersection()->getID() ? 
+                                        _currStreet->getOutIntersection() : 
+                                        _currStreet->getInIntersection();
 
             double x1, y1, x2, y2, xv, yv, dx, dy, l;
             i1->getPosition(x1, y1);
@@ -69,15 +73,18 @@ void Vehicle::drive()
             dx = x2 - x1;
             dy = y2 - y1;
             l = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (x1 - x2));
-            xv = x1 + completion * dx; // new position based on line equation in parameter form
+            // new position based on line equation in parameter form
+            xv = x1 + completion * dx; 
             yv = y1 + completion * dy;
             this->setPosition(xv, yv);
 
-            // check wether halting position in front of destination has been reached
+            // check wether halting position in front of 
+            // destination has been reached
             if (completion >= 0.9 && !hasEnteredIntersection)
             {
                 // request entry to the current intersection (using async)
-                auto ftrEntryGranted = std::async(&Intersection::addVehicleToQueue, _currDestination, get_shared_this());
+                auto ftrEntryGranted = std::async(&Intersection::addVehicleToQueue, 
+                                        _currDestination, get_shared_this());
 
                 // wait until entry has been granted
                 ftrEntryGranted.get();
@@ -91,14 +98,17 @@ void Vehicle::drive()
             if (completion >= 1.0 && hasEnteredIntersection)
             {
                 // choose next street and destination
-                std::vector<std::shared_ptr<Street>> streetOptions = _currDestination->queryStreets(_currStreet);
+                std::vector<std::shared_ptr<Street>> streetOptions = 
+                                    _currDestination->queryStreets(_currStreet);
                 std::shared_ptr<Street> nextStreet;
                 if (streetOptions.size() > 0)
                 {
-                    // pick one street at random and query intersection to enter this street
+                    // pick one street at random and query 
+                    // intersection to enter this street
                     std::random_device rd;
                     std::mt19937 eng(rd());
-                    std::uniform_int_distribution<> distr(0, streetOptions.size() - 1);
+                    std::uniform_int_distribution<> distr(0, 
+                                                        streetOptions.size() - 1);
                     nextStreet = streetOptions.at(distr(eng));
                 }
                 else
@@ -108,9 +118,12 @@ void Vehicle::drive()
                 }
                 
                 // pick the one intersection at which the vehicle is currently not
-                std::shared_ptr<Intersection> nextIntersection = nextStreet->getInIntersection()->getID() == _currDestination->getID() ? nextStreet->getOutIntersection() : nextStreet->getInIntersection(); 
+                std::shared_ptr<Intersection> nextIntersection = 
+                nextStreet->getInIntersection()->getID() == _currDestination->getID() ?
+                nextStreet->getOutIntersection() : nextStreet->getInIntersection(); 
 
-                // send signal to intersection that vehicle has left the intersection
+                // send signal to intersection that vehicle 
+                // has left the intersection
                 _currDestination->vehicleHasLeft(get_shared_this());
 
                 // assign new street and destination
