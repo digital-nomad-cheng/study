1d09fd7ec7a187dca26a0bcc9226f75e0d67efe2
vincent <vincentcheng@929@gmail.com>
2020-02-11 11:26:32 +0800

    udacity c++

---
+++ cpp/udacity_cpp/05_concurrency/garbage_collector_thread/main.cpp
@@ -0,0 +1,21 @@
+#include "gc_pointer_thread.hpp"
+#include "leak_tester.hpp"
+
+int main()
+{
+    Pointer<int> p = new int(19);
+    
+    p.showList();
+    p = new int(21);
+    p.showList();
+    p = new int(28);
+    p.showList();
+    
+    Pointer<int> q = new int(9);
+    std::cout << "q.showList(): " << std::endl;
+    q.showList();
+    p = q;
+    std::cout << "p.showList(): " << std::endl;
+    p.showList();
+    return 0;
+}
