bb291c8f2583d24343656d12a2066debb879b8ff
vincent <vincentcheng929@gmail.com>
2020-05-05 13:15:29 +0800

    openmp

---
+++ parallel_computing/openmp/hello_world_openmp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main()
+{
+    #pragma omp parallel
+    {
+        int ID = omp_get_thread_num();
+        printf("Hello(%d)", ID);
+        printf("World(%d)\n", ID);
+    }
+}
+
