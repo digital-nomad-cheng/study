ca4439a49c7b7003d67a4f1245421e45825ce06d
vincent <vincentcheng929@gmail.com>
2020-07-14 20:47:22 +0800

    Add chapter 6 load image texture

---
+++ opengl/LearnOpenGL/Libs/stb_image.c
@@ -0,0 +1,11 @@
+//
+//  stb_image.c
+//  LearnOpenGL
+//
+//  Created by yuhua.cheng on 2020/7/14.
+//  Copyright © 2020 idealabs. All rights reserved.
+//
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
