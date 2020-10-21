2a959f096138bd105ae0fe0e65c15dbc84d727c6
vincent <vincentcheng929@gmail.com>
2020-10-21 21:00:15 +0800

    Update assign 1

---
+++ cpp/cs106x/assignments/assign-1-game-of-life/src/life.cpp
@@ -0,0 +1,44 @@
+/**
+ * File: life.cpp
+ * --------------
+ * Implements the Game of Life.
+ */
+
+#include <iostream>  // for cout
+using namespace std;
+
+#include "console.h" // required of all files that contain the main function
+#include "simpio.h"  // for getLine
+#include "gevents.h" // for mouse event detection
+#include "strlib.h"
+
+#include "life-constants.h"  // for kMaxAge
+#include "life-graphics.h"   // for class LifeDisplay
+
+/**
+ * Function: welcome
+ * -----------------
+ * Introduces the user to the Game of Life and its rules.
+ */
+static void welcome() {
+    cout << "Welcome to the game of Life, a simulation of the lifecycle of a bacteria colony." << endl;
+    cout << "Cells live and die by the following rules:" << endl << endl;
+    cout << "\tA cell with 1 or fewer neighbors dies of loneliness" << endl;
+    cout << "\tLocations with 2 neighbors remain stable" << endl;
+    cout << "\tLocations with 3 neighbors will spontaneously create life" << endl;
+    cout << "\tLocations with 4 or more neighbors die of overcrowding" << endl << endl;
+    cout << "In the animation, new cells are dark and fade to gray as they age." << endl << endl;
+    getLine("Hit [enter] to continue....   ");
+}
+
+/**
+ * Function: main
+ * --------------
+ * Provides the entry point of the entire program.
+ */
+int main() {
+    LifeDisplay display;
+    display.setTitle("Game of Life");
+    welcome();
+    return 0;
+}
